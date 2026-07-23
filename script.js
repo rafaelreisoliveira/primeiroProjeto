2973582c60b2938a9614d166d4639b7827168df5
Rafael O Reis <rafaelreisnuvem@gmail.com>
2026-07-23 10:52:29 -0300

    Página com definição e botão.

---
+++ script.js
@@ -0,0 +1,4 @@
+function explicacao() {
+    let texto = document.getElementById("caracteristica");
+    texto.innerHTML = "Importante ferramenta de CI/CD (integração contínua/implantação contínua).";
+}
